0ced9aa7a77e7f9a2afe1fbce84b11ae85fbb5dd
Peng Li <seudut@gmail.com>
2016-12-05 22:23:16 +0800

    draft of setting modeline for active window only

--- perspeen-tab.el
+++ perspeen-tab.el
@@ -38,36 +38,67 @@
 (window-header-line-height)
 
 
-(setq header-line-format mode-line-format)
-(setq header-line-format (sd/header-line))
+(setq header-line-format nil)
+(setq mode-line-format (sd/header-line))
 
+;; (set-face-attribute 'header-lin)
 
+(set-face-attribute 'mode-line-inactive nil
+                    :underline nil
+                    :background (face-background 'default))
 
 (defun sd/header-line ()
   (let* ((bl (or (buffer-file-name) (buffer-name)))
          (len (length bl) )
          ;; (width (* (window-total-width) (/ (float 18) 13)))
 	 (width (window-total-width)))
-    ;; (make-string (- (round width) 10) ?-)
-    (make-string (- width 3) ?-)))
+    (when (powerline-selected-window-active)
+      ;; (make-string (- (round width) 10) ?-)
+      (make-string (- width 3) ?*))))
 
 
-(mapconcat 'identity '("foo" "bar") " ")
+;; (mapconcat 'identity '("foo" "bar") " ")
 
-(message (make-string 10 ?*))
+;; (message (make-string 10 ?*))
 
-(setq header-line-format mode-line-format)
+;; (setq header-line-format mode-line-format)
 
 (defun perspeen-tab-switch-to-buffer (buf-or-name &optional norecord force-same-window)
   "Advice to switch buffer, to update header line"
-  (setq header-line-format
-	'(:eval (sd/header-line) ;; (substring my-header
-		;; 	   (min (length my-header)
-		;; 		(window-hscroll)))
-		 )))
+  (setq mode-line-format
+	
+	'(:eval
+	  ;; (when (powerline-selected-window-active))
+	  (sd/header-line) ;; (substring my-header
+	  ;; 	   (min (length my-header)
+	  ;; 		(window-hscroll)))
+	  ))
+   ;; (my-update-header)
+  )
 
 (advice-add 'switch-to-buffer :after #'perspeen-tab-switch-to-buffer)
 
 
+
 (provide 'perspeen-tab)
+
+
 ;;; perspeen-tab.el ends here
+(defun my-update-header ()
+  (mapc
+   (lambda (window)
+     (with-current-buffer (window-buffer window)
+       ;; don't mess with buffers that don't have a header line
+       (when header-line-format
+         (let ((original-format (get 'header-line-format 'original))
+               ;; (inactive-face 'warning)
+	       (inactive-face 'mode-line-inactive)) ; change this to your favorite inactive header line face
+           ;; if we didn't save original format yet, do it now
+           (when (not original-format)
+             (put 'header-line-format 'original header-line-format)
+             (setq original-format header-line-format))
+           ;; check if this window is selected, set faces accordingly
+           (if (eq window (selected-window))
+               (setq header-line-format original-format)
+             (setq header-line-format `(:propertize ,original-format face ,inactive-face)))))))
+   (window-list)))
