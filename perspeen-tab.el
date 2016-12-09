cb6ee1cda7e63afb501dd3bed10898d3b1997394
Peng Li <seudut@gmail.com>
2016-12-09 22:11:57 +0800

    header line tringle

--- perspeen-tab.el
+++ perspeen-tab.el
@@ -37,40 +37,77 @@
   "Return the upper-left most window"
   (window-at (frame-width) 0))
 
+(defun sd/construct-header-line ()
+  "Generate header line"
+  (let* ((active (powerline-selected-window-active))
+	 (first-window (frame-first-window))
+	 (top-right-window (sd/get-upper-left-most-window))
+	 (current-window (selected-window))
+	 (separator-right (intern (format "powerline-%s-%s"
+					  (powerline-current-separator)
+					  (cdr powerline-default-separator-dir))))
+	 (separator-left (intern (format "powerline-%s-%s"
+					 (powerline-current-separator)
+					 (car powerline-default-separator-dir))))
+	 (lhs (list
+	       (powerline-raw (format "%s" "==first buffer== ") 'sd/powerline-active1 'l)
+	       (funcall separator-left 'sd/powerline-active1 'powerline-active1)))
+	 (rhs (list
+	       (funcall separator-right  'powerline-active1 'sd/powerline-active1)
+	       (powerline-raw (format-time-string " %Y-%m-%d %I:%M %p %a ") 'sd/powerline-active1 'r))))
+     (cond ((eq current-window first-window)
+	   (if (eq first-window top-right-window)
+	       (concat
+		(powerline-render lhs)
+		(powerline-fill 'powerline-active1 (powerline-width rhs))
+		(powerline-render rhs))
+	     (concat
+	      (powerline-render lhs)
+	      (powerline-fill 'powerline-active1 0))))
+	  ((eq current-window top-right-window)
+	   (concat
+	    (powerline-fill 'powerline-active1 (powerline-width rhs))
+	    (powerline-render rhs)))
+	  (t
+	   nil))))
 
-(defun sd/set-header-line-format ()
+(defun sd/set-header-line-format (&optional force)
   "Set the header line format"
-  (setq-default header-line-format
-		'(:eval
-		  (let* ((active (powerline-selected-window-active))
-			 (first-window (frame-first-window))
-			 (top-right-window (sd/get-upper-left-most-window))
-			 (current-window (selected-window))
-			 (lhs (list
-			       (powerline-raw (format "%s" "==first buffer==") 'sd/powerline-active1 'l)))
-			 (rhs (list
-			       (powerline-raw (format-time-string " %I:%M %p ") 'sd/powerline-active1 'r))))
-		    (if (eq first-window top-right-window)
-			(concat
-			 (powerline-render lhs)
-			 (powerline-fill 'powerline-inactive1 (powerline-width rhs))
-			 (powerline-render rhs))
-		      
-		      (cond ((eq current-window first-window)
-			     (concat
-			      (powerline-render lhs)
-			      (powerline-fill 'powerline-inactive1 0)))
-			    ((eq current-window top-right-window)
-			     (concat
-			      (powerline-fill 'powerline-inactive1 (powerline-width rhs))
-			      (powerline-render rhs)))
-			    (t
-			     nil)))))))
+  (if force
+      (setq header-line-format
+		    '(:eval
+		      (sd/construct-header-line)))
+    (setq header-line-format
+		    '(:eval
+		      (sd/construct-header-line)))))
 
 (sd/set-header-line-format)
 ;; (setq header-line-format nil)
 
+(defun sd/switch-to-buffer-header-line (buf-or-name &optional norecord force-same-window)
+  "Advice"
+  (when buf-or-name
+    (message "====switch--buffer===")
+    (sd/set-header-line-format)))
+;; (advice-add 'switch-to-buffer :after #'sd/switch-to-buffer-header-line)
 
+;; (add-hook 'after-change-major-mode-hook #'sd/set-header-line-format)
+
+(defun sd/haha ()
+  "jfieojfo"
+  (message "-----test------"))
+
+(add-hook 'change-major-mode-hook #'sd/set-header-line-format)
+
+(mapc (lambda (window)
+	(with-selected-window window
+	  (sd/set-header-line-format)))
+      (window-list))
+
+(force-window-update t)
+(walk-windows (lambda (window)
+		(with-selected-window window
+		  (sd/set-header-line-format t))))
 
 (provide 'perspeen-tab)
 
